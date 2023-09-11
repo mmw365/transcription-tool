c61964d8c917b052150191dcea509b64a8a4b339
makoto <mmmagic@gmail.com>
2023-09-11 21:52:22 +0900

    added top p

--- process_audio.py
+++ process_audio.py
@@ -107,7 +107,9 @@ def send_to_gpt(transcript, filename_sum_text):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         # model="gpt-4",
-        messages=messages
+        messages=messages,
+        # temperature=0.1,
+        top_p=0.1,
     )
     res = response["choices"][0]["message"]["content"]
     print("--- 要約 ---")
